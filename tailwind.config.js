0523f07d4b5f4058fba8d3f033e92a37c3a9de09
Rohit Singh <162619344+rohit-singh4@users.noreply.github.com>
2024-05-05 00:21:26 +0530

    Add files via upload
    added new file  of the new portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/assets/text-bg.jpg')",
+      },
+     
+    },
+  },
+  plugins: [],
+};
